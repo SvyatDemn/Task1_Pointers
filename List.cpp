9485f9f8448e7e6e51c73dab293f7a7eb611db67
StMarian <repolevedem@gmail.com>
2017-03-26 12:40:01 +0300

    Task 1

---
+++ List.cpp
@@ -0,0 +1,159 @@
+#include "List.h"
+
+typedef unsigned int ui;
+
+const ui SizeOfAr = 10;
+const ui SizeOfInf = 2;
+
+void StringListInit(char*** list)
+{
+	void* tmp_ptr = malloc(SizeOfAr * sizeof(char*) + SizeOfInf * sizeof(ui));
+			memset(tmp_ptr, 0, SizeOfAr * sizeof(char*) + SizeOfInf * sizeof(ui));
+
+			unsigned* capacity = (unsigned*)tmp_ptr;
+			unsigned* size = (unsigned*)tmp_ptr + 1;
+			*capacity = 10;
+			*list = (char**)(capacity + 2);
+
+}
+
+void StringListDestroy(char*** list)
+{
+	if (list != nullptr)
+	{
+	ui size = StringListSize(*list);
+	for (int i = 0; i < size; i++)
+		{
+		free((*list)[i]);
+		(*list)[i] = nullptr;
+		}
+    }
+}
+
+ui StringListSize(char** list)
+{
+	if (list != nullptr)
+		return (ui)list[-1];
+	else
+		return -1;
+}
+
+ui StringListCapacity(char** list)
+{
+	if (list != nullptr)
+		return (ui)list[-2];
+	else
+		return -1;
+}
+
+void ShowList(char** list)
+{
+	cout << "Capacity: " << StringListCapacity(list) << endl;
+	cout << "Size" << StringListSize(list) << endl;
+	ui size = StringListSize(list);
+	for(int i = 0; i < size; i++)
+		cout << "Number: " << i << ':' << list[i] << endl;
+}
+
+void StringListAdd(char*** list, char* str)
+{
+	if (list != nullptr)
+	{
+		ui size = StringListSize(*list);
+		if(StringListSize(*list) == StringListCapacity(*list))
+		{
+			ui newcapacity = size * 2;
+			void* newmem = (*list) - 2;
+			newmem = realloc(newmem, newcapacity * sizeof(char**) + SizeOfInf * sizeof(ui));
+
+			if(newmem != nullptr)
+			{
+				*list = (char**)(((ui*)(newmem)) + 2);
+				memset((*list) + size + 1, 0, newcapacity - size);
+			}
+			size = (ui) (*list)[-1]++;
+			*list[size] = (char*)malloc(strlen(str) + 1);
+			strcpy(*list[size], str);
+		}
+
+
+	}
+}
+
+void StringListRemove(char** list, char* str)
+{
+	if (list != nullptr)
+	{
+		ui size = StringListSize(list);
+		ui index = StringListIndexOf(list, str);
+		while(index >= 0)
+		{
+			for(int i = index; i <= size - 2; i++)
+			{
+				Swap(&list[i], &list[i + 1]);
+			}
+			list[-1]--;
+			free(list[size - 1]);
+			list[size - 1] = nullptr;
+		}
+	}
+}
+
+ui StringListIndexOf(char** list, char* str)
+{
+	if (list != nullptr)
+	{
+	ui size = StringListSize(list);
+		if (list != nullptr)
+			for (int i = 0; i < size; i++)
+				if (strcmp(list[i], str) == 0)
+					return i;
+	}
+}
+
+void Swap(char** a, char** b)
+{
+	char* temporary_pointer = *a;
+	*a = *b;
+	*b = temporary_pointer;
+}
+
+void StringListRemoveDuplicates(char** list)
+{
+	if (list != nullptr)
+	{
+		for (int i = 0; i < StringListSize(list) - 1; i++)
+			for (int j = i + 1; j < StringListSize(list); j++)
+				if (!strcmp(list[i], list[j]))
+					{
+						StringListRemove(list, list[j]);
+						j--;
+					}
+	}
+}
+
+
+void StringListReplaceInStrings(char** list, char* before, char* after)
+{
+	if (list != nullptr)
+	{
+		int index = StringListIndexOf(list, before);
+		while (index >= 0)
+		{
+			list[index] = (char*)realloc(list[index], strlen(after) + 1);
+			strcpy(list[index], after);
+		}
+	}
+}
+
+void StringListSort(char** list)
+{
+	if (list != nullptr)
+	{
+		for(int i = 0; i < StringListSize(list) - 1; i++)
+		{
+			for(int j = i + 1; j < StringListSize(list); j++)
+				if(strcmp(list[i], list[j]) > 0)
+		}
+	}
+}
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "List.h"
+#include "List.cpp"
+
+int main()
+{
+	/*
+	char** list = nullptr;
+
+	StringListInit(&list);
+	StringListAdd(&list, "abc");
+	StringListAdd(&list, "abcd");
+	StringListAdd(&list, "abcde");
+	StringListAdd(&list, "abcde");
+	StringListAdd(&list, "abcgf");
+	StringListAdd(&list, "abcgr");
+	StringListAdd(&list, "abc");
+	StringListAdd(&list, "abcgy");
+	StringListAdd(&list, "abcgu");
+	StringListAdd(&list, "abcgi");
+	ShowList(list);
+	*/
+
+
+
+}
